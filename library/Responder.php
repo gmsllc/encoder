6addecb98c2becdd1ec197dabfcd2056123aa56c
Lloyd <lloyd@dreamstarcash.com>
2012-04-19 11:20:34 +0200

    added logging

--- library/Responder.php
+++ library/Responder.php
@@ -1,10 +1,11 @@
 <?php
 class Responder {
 
-    public function __construct($config, $db){
+    public function __construct($config, $db, $log_file=false){
         $this->config = $config;
         $this->sql = $this->config['sql'];
         $this->db = $db;
+        $this->log_file = $log_file;
     }
 
     public function handleResponse($xml){
@@ -12,7 +13,18 @@ class Responder {
         $stmt = $this->db->prepare($this->sql['update_status']);
         $status = (string)$xml->status;
         $media_id = (int)$xml->mediaId;
+        $this->logResponse($status, $media_id);
         $stmt->execute(array(':status' => $status, ':media_id' => $media_id));
     }
+
+    protected function logResponse($status, $media_id){
+        if ($this->log_file) {
+            $timestamp = date("Y-m-d:h-i-s");
+            $message_to_log = "[$timestamp] MediaId: $media_id, Status: $status";
+            file_put_contents($this->log_file, $message_to_log, FILE_APPEND);
+        }
+
+    }
+
 }
 
