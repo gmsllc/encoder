481b89e2e6ec808159d27e7da05fe62261bc0c1a
Lloyd Moore <lloyd@dreamstarcash.com>
2012-04-25 15:34:51 +0200

    removed now unneeded buildXml test

--- tests/testEncoderApi.php
+++ tests/testEncoderApi.php
@@ -27,17 +27,10 @@ class TestEncoderApi extends Zend_Test_PHPUnit_ControllerTestCase {
         unset($this->encoder);
     }
 
-    public function testBuildXml(){
-        $data = $this->dummyData();
-        $xml_real = $this->encoder->buildXml($data, $this->source, $this->destination);
-        $xml_fake = $this->dummyXml();
-        $this->assertContains($xml_fake, $xml_real);
-    }
-
     public function testMakeApiRequest(){
         $config = $this->encoder->getConfiguration();
         $source_video = $config['xml_data']['source']; 
-        $destination  = $config['xml_data']['destination'];
+        $destination  = $config['xml_data']['format']['destination'];
         $priority = 1;
         $expected = trim(self::CORRECT_XML_RESPONSE);
         $response = $this->encoder->requestEncoding($source_video, $destination, $priority);
@@ -49,13 +42,15 @@ class TestEncoderApi extends Zend_Test_PHPUnit_ControllerTestCase {
             'foo' => 'A',
             'bar' => 'B',
             'source' => $this->source,
-            'destination' => $this->destination,
+	    'format' => array(
+		    'destination' => $this->destination
+	    ),
             'more' => array('fux' => 'C', 'sum' => 'D')
         );
     }
 
     protected function dummyXml(){
-        return "<query><foo>A</foo><bar>B</bar><source>{$this->source}</source><destination>{$this->destination}</destination><more><fux>C</fux><sum>D</sum></more></query>";
+        return "<query><foo>A</foo><bar>B</bar><source>{$this->source}</source><format><destination>{$this->destination}</destination></format><more><fux>C</fux><sum>D</sum></more></query>";
     }
 
     const CORRECT_XML_RESPONSE =<<<CORRECT
